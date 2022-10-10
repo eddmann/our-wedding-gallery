024fc8dca018cef886ff8ddbd721fb0b0a9da0af
Edd Mann <the@eddmann.com>
2022-10-10 18:03:39 +0100

    Handle lazy loaded images within lightbox

--- client/src/component/PhotoGallery.js
+++ client/src/component/PhotoGallery.js
@@ -15,28 +15,42 @@ function PhotoGallery({ initialUrl }) {
   const doFetch = useCallback(async () => {
     const response = await fetch(nextUrl).then(response => response.json());
 
-    setPhotos(photos => [...photos, ...response.photos].map((photo, idx) => ({ idx, ...photo })));
+    setPhotos(photos => [...photos, ...response.photos]);
     setNextUrl(response?._links?.next?.href);
+
+    return response.photos.length > 0;
   }, [nextUrl]);
 
   return (
     <>
       <InfiniteScroll loadMore={doFetch} hasMore={!!nextUrl} loader={<LoadingBar />}>
         <div className="grid gap-1 grid-cols-2 md:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-8">
-          {photos.map(photo => (
+          {photos.map((photo, idx) => (
             <img
               key={photo.id}
               className="flex object-cover w-full h-full cursor-pointer"
               alt=""
               src={photo.thumbnail}
-              onClick={() => setSelected(photo)}
+              onClick={() => setSelected(idx)}
             />
           ))}
         </div>
       </InfiniteScroll>
       <Lightbox
-        photo={selected}
-        onNext={() => setSelected(photos[selected.idx + 1 >= photos.length ? 0 : selected.idx + 1])}
+        photo={selected !== undefined && photos[selected]}
+        onNext={() => {
+          if (selected + 1 < photos.length) {
+            setSelected(selected + 1);
+            return;
+          }
+
+          if (!nextUrl) {
+            setSelected(0);
+            return;
+          }
+
+          doFetch().then(hasMorePhotos => setSelected(hasMorePhotos ? selected + 1 : 0));
+        }}
         onClose={() => setSelected(undefined)}
       />
     </>
